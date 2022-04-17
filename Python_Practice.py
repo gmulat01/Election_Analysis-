d7f18107099782de3f99cc113a6c4cade72fe944
Gadise Mulat <gtmulat@gmail.com>
2022-04-17 17:57:50 -0500

    Election Results

---
+++ Python_Practice.py
@@ -0,0 +1,34 @@
+print("Hello World")
+5+2*3
+(5+2)*3
+
+5+2
+
+counties = ["Arapahoe", "Denver", "Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+
+    if counties[3] != 'Jefferson':
+        print(counties[2])
+        
+temperature = int(input("What is the temperature outside?"))
+if temperature > 80:
+        print("Turn on the AC.")
+
+else: 
+        print("Open the windows")
+
+score = int(input("What is your test score?"))
+
+if score >= 90:
+    print('Your grade is an A')
+elif score >= 80:
+        print('Your grade is a B')
+    elif score >= 70:
+            print('Your grade is a C')
+                 elif score >=60:
+                         print('Your grade is a D')
+                         elif: 
+                             print('Your grade is an F')
+
+                             
